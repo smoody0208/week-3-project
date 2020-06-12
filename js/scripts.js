e33d50e256a3f0cb1883356bc20a6bb646536a75
Spencer Moody <spencer.moody@outlook.com>
2020-06-12 10:43:29 -0700

    Add branching for each wordarray and index

--- js/scripts.js
+++ js/scripts.js
@@ -2,16 +2,20 @@ function numberInput(number){
   let result = [];
   let numberArray = [1, 2, 3];
   let wordArray = ["beep", "boop", "won't you be my neighbor?"];
-
-  for(let index = 0; index <= number.length; index ++) {
-    if (index.includes(numberArray[2])){
-    result.push(" " + wordArray[2]);
-  } else if (index.includes(numberArray[1])) {
-    result.push(" " + wordArray[1]);
-  }
   
-    }
-  };
+  for(let index = 0; index <= number; index ++) {
+    if (index.toString().includes(numberArray[2])){
+    result.push(" " + wordArray[2]);
+  } else if (index.toString().includes(numberArray[1])) {
+    result.push(" " + wordArray[1]);
+  } else if (index.toString().includes(numberArray[0])) {
+    result.push(" " + wordArray[0])
+  } else {
+    result.push (" " + index);
+  }
+}
+return result;
+}
 
 $(document).ready(function(){
   $("form#formOne").submit(function(event){
@@ -20,4 +24,4 @@ $(document).ready(function(){
     let number = numberInput(result);
     $("#result").text(number);
   })
-})
+}) 

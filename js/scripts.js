0521c1261cfa83bb8a7cd3798ffde3f34f2f8a5a
Spencer Moody <spencer.moody@outlook.com>
2020-06-12 08:34:53 -0700

    Add numberInput function

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+function numberInput(number){
+  let result = [];
+  let numberArray = [1, 2, 3];
+  let wordArray = ["beep", "boop", "won't you be my neighbor?"];
+
+  for(let index = 0; index <= number.length; index ++) {
+    if (index.includes(numberArray[2])){
+    result.push(" " + wordArray[2]);
+  } else if 
+    
+  }
+}

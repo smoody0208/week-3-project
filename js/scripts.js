79d700907ea8b9943aa37af2b575be5aacecdfd9
Spencer Moody <spencer.moody@outlook.com>
2020-06-12 11:02:55 -0700

    Clean up branching and formatting

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,22 @@
-function numberInput(number, name){
+function numberInput(number, name) {
   let result = [];
   let numberArray = [1, 2, 3];
-  let wordArray = ["Beep", "Boop", "Won't you be my neighbor?"];
+  let wordArray = ["Beep", "Boop", "Won't you be my neighbor"];
   
   for (let index = 0; index <= number; index ++) {
     if (index % 3 === 0) {
-    result.push(" " + wordArray[2] + name + " " ) 
-  } else if (index.toString().includes(numberArray[2])){
-    result.push(" " + wordArray[2]);
-  } else if (index.toString().includes(numberArray[1])) {
-    result.push(" " + wordArray[1]);
-  } else if (index.toString().includes(numberArray[0])) {
-    result.push(" " + wordArray[0])
-  } else {
-    result.push (" " + index);
+      result.push(" " + wordArray[2] + ", " + name + "?" ) 
+    } else if (index.toString().includes(numberArray[2])) {
+      result.push(" " + wordArray[2]);
+    } else if (index.toString().includes(numberArray[1])) {
+      result.push(" " + wordArray[1]);
+    } else if (index.toString().includes(numberArray[0])) {
+      result.push(" " + wordArray[0])
+    } else {
+      result.push (" " + index);
+    }
   }
-}
-return result;
+  return result;
 }
 
 $(document).ready(function(){
@@ -26,5 +26,5 @@ $(document).ready(function(){
     let name = $("input#name").val();
     let number = numberInput(result, name);
     $("#result").text(number);
-  })
-}) 
+  });
+}) ;
